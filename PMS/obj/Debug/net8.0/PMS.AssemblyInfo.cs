b9da80275ef3278fc0c3f6c97dfe307c06da2119
hamzajaved5656 <hamzajaved5656@gmail.com>
2024-09-08 16:37:52 +0500

    added new code

--- PMS/obj/Debug/net8.0/PMS.AssemblyInfo.cs
+++ PMS/obj/Debug/net8.0/PMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6271a0a3eb51d7d74dfc44822d5788d117df74f")]
 [assembly: System.Reflection.AssemblyProductAttribute("PMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
